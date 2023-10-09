18f3a735057158b699678ab850de4e46085945ad
Asst2Mangr <124299473+Asst2Mangr@users.noreply.github.com>
2023-10-09 11:31:13 -0500

    getting deck controller set up

---
+++ Controllers/DeckController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project2.Models;
+
+namespace Project2.Controllers
+{
+    public class DeckController : Controller
+    {
+        private DeckContext context { get; set; }
+
+        public DeckController(DeckContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
