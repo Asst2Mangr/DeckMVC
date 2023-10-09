5cb28d857b62fcc3fb457741d51664869811e5a0
Asst2Mangr <124299473+Asst2Mangr@users.noreply.github.com>
2023-10-09 11:38:02 -0500

    finishing Deck Controller

--- Controllers/DeckController.cs
+++ Controllers/DeckController.cs
@@ -15,5 +15,57 @@ namespace Project2.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.Deck = context.Decks.OrderBy(d => d.DeckName).ToList();
+            return View("Edit", new Deck());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.Deck = context.Decks.OrderBy(d => d.DeckName).ToList();
+            var deck = context.Decks.Find(id);
+            return View(deck);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Deck deck)
+        {
+            if (ModelState.IsValid)
+            {
+                if (deck.DeckID == 0)
+                    context.Decks.Add(deck);
+                else
+                    context.Decks.Update(deck);
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Action = (deck.DeckID == 0) ? "Add" : "Edit";
+                ViewBag.Decks = context.Decks.OrderBy(d => d.DeckName).ToList();
+                return View(deck);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var deck = context.Decks.Find(id);
+            return View(deck);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Deck deck)
+        {
+            context.Decks.Remove(deck);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
